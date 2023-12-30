a4c9785cc0141dddc66cbdec57db71127ef483ac
Forrest Erickson <forresterickson@comcast.net>
2023-12-29 21:05:59 -0500

    Add program name. Bracket startup with LED high.

--- UART3Loopback.ino
+++ UART3Loopback.ino
@@ -1,18 +1,26 @@
 // UART3Loopback
 // Inspired by code here: https://arduino-er.blogspot.com/2015/04/arduino-due-muilt-serial-and-serialevent.html
+// Type stuff in the Serial Monitor (UART0) and it is forwared to UART1
+// All other UART input is forwared out the Serial Monitor (UART0)
 
+#define COMPANY_NAME "pubinv.org "
+#define PROG_NAME "UART3Loopback"
+#define VERSION ":V0.1"
+#define DEVICE_UNDER_TEST "Hardware:_Control_V1.1_Firmware:_"  //A model number
+#define LICENSE "GNU Affero General Public License, version 3 "
 
 
 int LED = 13;
 boolean LEDst = true;
 
 void setup() {
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, HIGH);
   Serial.begin(115200);
   Serial1.begin(4800);
   Serial2.begin(4800);
   Serial3.begin(9600);
-  pinMode(LED, OUTPUT);
-  digitalWrite(LED, LEDst);
+  digitalWrite(LED, LOW);
 }
 
 void loop() {
@@ -23,7 +31,7 @@ void loop() {
 //    Serial2.write(a); // Second power supply
 //    //Serial3.write(a);
 //  }
-//  
+
 }
 
 //UART0
